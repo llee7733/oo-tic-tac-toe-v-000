8536b5a38ce5cfc333c3e1666a12afe684127caa
Lloyd Lee <kinglloydlee@gmail.com>
2018-07-18 19:43:43 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -77,5 +77,5 @@ def won?
     position_3 = @board[win_index_3]
     
 (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
-
-+      end
+}
+end
