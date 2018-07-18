1a696fa55cb4b2c91ead2a846209b8a0ac1b9687
Lloyd Lee <kinglloydlee@gmail.com>
2018-07-18 19:39:32 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -52,4 +52,11 @@ end
 
 def turn_count
   turn_counter = 0
-  
+  @board.each do |position|
+    if position == "X" || position == "O"
+      turn_counter += 1
+    end
+  end
+  turn_counter
+end
+
